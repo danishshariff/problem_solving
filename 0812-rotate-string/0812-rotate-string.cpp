f0f349014cfb117621cc5a46866632dfd061099d
danishshariff <132253347+danishshariff@users.noreply.github.com>
2024-11-03 12:35:23 +0530

    Time: 0 ms (100%), Space: 7.6 MB (55.81%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-     int n=s.length();
-     if(s==goal) return true;
-    if(n!=goal.length()) return false;
-    for(int k=1;k<n;k++){
-     string st=s;
-     reverse(st.begin(),st.begin()+k);
-     reverse(st.begin()+k,st.end());
-     reverse(st.begin(),st.end());
-     if(st==goal) return true;
-    }
-    return false; 
+    //  int n=s.length();
+    //  if(s==goal) return true;
+    // if(n!=goal.length()) return false;
+    // for(int k=1;k<n;k++){
+    //  string st=s;
+    //  reverse(st.begin(),st.begin()+k);
+    //  reverse(st.begin()+k,st.end());
+    //  reverse(st.begin(),st.end());
+    //  if(st==goal) return true;
+    // }
+    // return false;                                                                           
+        // Check if `goal` can be a rotation of `s`
+        return (s.length() == goal.length()) && ((s + s).find(goal) != string::npos);
+    
     }
 };
