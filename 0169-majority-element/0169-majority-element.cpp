08d37e9047ae0f264190bbac317b227dbf8382e5
danishshariff <132253347+danishshariff@users.noreply.github.com>
2024-11-04 17:29:18 +0530

    Time: 0 ms (100%), Space: 26.9 MB (79.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,16 +1,44 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-     map<int,int> mpp;
+    //  map<int,int> mpp;
      int n=nums.size();
-     for(int i=0;i<n;i++){
-        mpp[nums[i]]++;
-     }
-     for(auto it: mpp){
-        if(it.second>n/2){
-            return it.first;
+    //  for(int i=0;i<n;i++){
+    //     mpp[nums[i]]++;
+    //  }
+    //  for(auto it: mpp){
+    //     if(it.second>n/2){
+    //         return it.first;
+    //     }
+    //  }
+    //     return -1;
+    //  }
+
+    //moores voting algo
+
+    int cnt=0;
+    int ele;
+    for(int i=0;i<n;i++){
+        if(cnt==0){
+            cnt=1;
+        ele=nums[i];
         }
-     }
-        return -1;
-     }
+        else if(nums[i]==ele){
+            cnt++;
+        }
+        else{
+            cnt--;
+        }
+    }
+    int cnt1=0;
+    for(int i=0;i<n;i++){
+        if(nums[i]==ele){
+            cnt1++;
+        }
+    }
+    if(cnt1>n/2){
+        return ele;
+    }
+    return -1;
+    }
 };
