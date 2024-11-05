6c86bbb6a57d7f21664716c774ae0854686ed609
danishshariff <132253347+danishshariff@users.noreply.github.com>
2024-11-05 11:25:31 +0530

    Time: 0 ms (100%), Space: 12 MB (96.18%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -2,27 +2,34 @@ class Solution {
 public:
     int minChanges(string s) {
         int n=s.length();
-       int odd_l= n%2==1;
+    //    int odd_l= n%2==1;
        int cnt=0;
-    //    if(s==odd_l) return -1;
-       for(int i=1;i<n;i++){
-        if(s[i-1]=='1' && s[i]=='1') {
-            i++;
-        }
-        else if(s[i-1]=='0' && s[i]=='0'){
-            i++;
-        }
-        else if(s[i-1]=='1' && s[i]=='0'){
-            s[i]='1';
-            i++;
+    // //    if(s==odd_l) return -1;
+    //    for(int i=1;i<n;i++){
+    //     if(s[i-1]=='1' && s[i]=='1') {
+    //         i++;
+    //     }
+    //     else if(s[i-1]=='0' && s[i]=='0'){
+    //         i++;
+    //     }
+    //     else if(s[i-1]=='1' && s[i]=='0'){
+    //         s[i]='1';
+    //         i++;
+    //         cnt++;
+    //     }
+    //     else if(s[i-1]=='0' && s[i]=='1'){
+    //         s[i]='0';
+    //         i++;
+    //         cnt++;
+    //     }
+    //    }
+    //    return cnt;
+    // }
+    for(int i=0;i<n;i+=2){
+        if(s[i]!=s[i+1]){
             cnt++;
         }
-        else if(s[i-1]=='0' && s[i]=='1'){
-            s[i]='0';
-            i++;
-            cnt++;
-        }
-       }
-       return cnt;
+    }
+        return cnt;
     }
 };
