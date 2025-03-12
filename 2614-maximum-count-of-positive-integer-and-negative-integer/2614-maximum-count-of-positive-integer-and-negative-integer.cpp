9b2665983e59e01bc9f1201985b8fac51bcdfd26
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-03-12 22:46:49 +0530

    Time: 0 ms (100%), Space: 21.4 MB (75.85%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,21 +1,63 @@
 class Solution {
 public:
-    int maximumCount(vector<int>& nums) {
-        int neg=0;
-        int pos=0;
+
+    //optimal is bs lb and ub functions
+
+    int lb(vector<int>& nums){
+        int l=0;
         int n=nums.size();
-        for(int i=0;i<n;i++){
-            if(nums[i]<0){
-                neg++;
+        int r=n-1;
+        int ind=nums.size();
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]<0){
+                l=mid+1;
             }
-            else if(nums[i]>0){ 
-                pos++;
+            else if(nums[mid]>=0){
+                r=mid-1;
+                ind=mid;
             }
-            else{
-                continue;
-            }
-            
         }
-        return max(neg,pos);
+        return ind;
+    }
+
+    
+    int ub(vector<int>& nums){
+        int l=0;
+        int n=nums.size();
+        int r=n-1;
+        int ind=nums.size();
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(nums[mid]<=0){
+                l=mid+1;
+            }
+            else if(nums[mid]>0){
+                r=mid-1;
+                ind=mid;
+            }
+        }
+        return ind;
+    }
+    int maximumCount(vector<int>& nums) {
+        // int neg=0;
+        // int pos=0;
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]<0){
+        //         neg++;
+        //     }
+        //     else if(nums[i]>0){ 
+        //         pos++;
+        //     }
+        //     else{
+        //         continue;
+        //     }
+            
+        // }
+        // return max(neg,pos);
+        int pos=nums.size()-ub(nums);
+        int neg=lb(nums);
+        return max(pos,neg);
     }
 };
