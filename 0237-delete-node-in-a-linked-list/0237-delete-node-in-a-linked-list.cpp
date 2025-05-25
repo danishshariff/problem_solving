a21ca9daddfa2173b41984284a14527df05b746d
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-05-25 14:03:03 +0530

    Time: 7 ms (52.18%), Space: 12.5 MB (18.36%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -9,21 +9,12 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        // Assume the node to be deleted is not the tail.
-        if(node == nullptr || node->next == nullptr) {
-            return; // Shouldn't happen based on the problem description.
+        if(node== NULL || node->next==NULL){
+            return;
         }
-        
-        // Copy the value of the next node to the current node
-        node->val = node->next->val;
-        
-        // Store a reference to the next node
-        ListNode* temp = node->next;
-        
-        // Bypass the next node
-        node->next = node->next->next;
-        
-        // Free the bypassed node (optional, not always required in LeetCode problems)
+        node->val=node->next->val;
+        ListNode* temp=node->next;
+        node->next=node->next->next;
         delete temp;
     }
 };
