a3c3e50618516af602927d61d43ff493ec3bec21
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-05-26 17:40:13 +0530

    Time: 4 ms (53.84%), Space: 124.1 MB (44.03%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,22 +10,59 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        stack<int> st;
-        ListNode* temp=head;
-        while(temp!=nullptr){
-            st.push(temp->val);
-            temp=temp->next;
+
+    ListNode* revell(ListNode* head){
+        if(head==NULL || head->next==NULL){
+            return head;
         }
-        temp=head;
-        while(temp!=nullptr){
-            if(temp->val!=st.top()){
+        ListNode* newhead=revell(head->next);
+        ListNode* front=head->next;
+        front->next=head;
+        head->next=nullptr;
+        return newhead;
+        
+    }
+    bool isPalindrome(ListNode* head) {
+
+        // brute using stack
+
+        // stack<int> st;
+        // ListNode* temp=head;
+        // while(temp!=nullptr){
+        //     st.push(temp->val);
+        //     temp=temp->next;
+        // }
+        // temp=head;
+        // while(temp!=nullptr){
+        //     if(temp->val!=st.top()){
+        //         return false;
+        //     }
+        //     st.pop();
+        //     temp=temp->next;
+
+        // }
+        // return true;
+
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast->next!=nullptr && fast->next->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        ListNode* newhead=revell(slow->next);
+        ListNode* first=head;
+        ListNode* second=newhead;
+
+        while(second!=nullptr){
+            if(first->val!=second->val){
+                revell(newhead);
                 return false;
             }
-            st.pop();
-            temp=temp->next;
+            first=first->next;
+            second=second->next;
 
         }
-        return true;
+            revell(newhead);
+            return true;
     }
 };
