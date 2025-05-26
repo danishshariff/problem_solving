078424da21b6575a74eb52e157a731a40bc762ed
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-05-26 22:03:03 +0530

    Time: 0 ms (100%), Space: 15 MB (25.09%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -14,31 +14,52 @@ public:
          if (head == NULL) {
         return NULL;
     }
-        ListNode* temp=head;
-        int cnt=0;
-        while(temp!=nullptr){
-            cnt++;
-            temp=temp->next;
-        }
 
-        int res=(cnt-n);
-        temp=head;
-        if (res == 0) {
-            ListNode* newHead = head->next;
-            delete head;
-            return newHead;
-        }
+    //brute
+        // ListNode* temp=head;
+        // int cnt=0;
+        // while(temp!=nullptr){
+        //     cnt++;
+        //     temp=temp->next;
+        // }
 
-        while(temp!=nullptr){
-            res=res-1;
-            if(res==0){
-                break;
-            }
-            temp=temp->next;
+        // int res=(cnt-n);
+        // temp=head;
+        // if (res == 0) {
+        //     ListNode* newHead = head->next;
+        //     delete head;
+        //     return newHead;
+        // }
 
+        // while(temp!=nullptr){
+        //     res=res-1;
+        //     if(res==0){
+        //         break;
+        //     }
+        //     temp=temp->next;
+
+        // }
+        // ListNode* dltnode=temp->next;
+        // temp->next=temp->next->next;
+        // delete(dltnode);
+        // return head;
+
+        //optimal
+
+        ListNode* fast=head;
+        ListNode* slow=head;
+        for(int i=0;i<n;i++){
+            fast=fast->next;
         }
-        ListNode* dltnode=temp->next;
-        temp->next=temp->next->next;
+        if(fast==nullptr){
+            return head->next;
+        }
+        while(fast->next!=nullptr){
+            fast=fast->next;
+            slow=slow->next;
+        }
+         ListNode* dltnode=slow->next;
+        slow->next=slow->next->next;
         delete(dltnode);
         return head;
     }
