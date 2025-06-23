462dd2221a3202a1ba643014360745e0eb09baec
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-06-23 14:47:40 +0530

    Time: 0 ms (100%), Space: 23.6 MB (92.58%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -12,16 +12,34 @@
 class Solution {
 public:
         int diameter=0;
-int maxDepth(TreeNode* root) {
-        if (!root) return 0;
-        int lefth=maxDepth(root->left);
-        int righth=maxDepth(root->right);
-        diameter=max(lefth+righth, diameter);
+// int maxDepth(TreeNode* root) {
+//         if (!root) return 0;
+//         int lefth=maxDepth(root->left);
+//         int righth=maxDepth(root->right);
+//         diameter=max(lefth+righth, diameter);
 
-        return 1 + max(lefth,righth);
+//         return 1 + max(lefth,righth);
+//    }
+// int diameterOfBinaryTree(TreeNode* root) {
+//         maxDepth(root);
+//         return diameter;
+//     }
+//optimal
+
+int maxheight(TreeNode* root, int& diameter){
+    if(root==NULL){
+        return 0;
     }
+    int lh=maxheight(root->left,diameter);
+    int rh=maxheight(root->right,diameter);
+    diameter=max(lh+rh,diameter);
+    return 1+max(lh,rh);
+
+}
+
+    
     int diameterOfBinaryTree(TreeNode* root) {
-        maxDepth(root);
+        maxheight(root,diameter);
         return diameter;
     }
 };
