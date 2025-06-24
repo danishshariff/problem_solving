866ab21740dbc9967893e2090d6fc635ea7bd4e2
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-06-24 11:52:18 +0530

    Time: 12 ms (40.92%), Space: 14.5 MB (90.46%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.cpp
@@ -1,23 +1,47 @@
 class Solution {
 public:
     vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
-       int n=nums.size();
-       vector<int> idx;
-       vector<int> ans;
+        //brute
+    //    int n=nums.size();
+    //    vector<int> idx;
+    //    vector<int> ans;
 
-       for(int i=0;i<n;i++){
-        if(nums[i]==key){
-            idx.push_back(i);
-        }
-       }
-       for(int i=0;i<n;i++){
-        for(int j=0;j<idx.size();j++){
-            if(abs(i-idx[j])<=k){
-                ans.push_back(i);
-                break;
+    //    for(int i=0;i<n;i++){
+    //     if(nums[i]==key){
+    //         idx.push_back(i);
+    //     }
+    //    }
+    //    for(int i=0;i<n;i++){
+    //     for(int j=0;j<idx.size();j++){
+    //         if(abs(i-idx[j])<=k){
+    //             ans.push_back(i);
+    //             break;
+    //         }
+    //     }
+    //    }
+    //    return ans;
+
+
+    //optimal
+     int n = nums.size();
+        vector<bool> marked(n, false);
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == key) {
+                int start = max(0, i - k);
+                int end = min(n - 1, i + k);
+                for (int j = start; j <= end; j++) {
+                    marked[j] = true;
+                }
             }
         }
-       }
-       return ans;
+
+        for (int i = 0; i < n; i++) {
+            if (marked[i]) ans.push_back(i);
+        }
+
+        return ans;
+    
     }
 };
