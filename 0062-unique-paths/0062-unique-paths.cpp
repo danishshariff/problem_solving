877fafcca7e5dabde1588230fbc7266c367c1a72
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-07-19 17:26:06 +0530

    Time: 0 ms (100%), Space: 8.4 MB (72.27%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -20,36 +20,49 @@
 
 //tab
 
+// class Solution {
+// public:
+//     int uniquePaths(int m, int n) {
+//         vector<vector<int>> dp(m, vector<int>(n, -1));
+//         dp[0][0]=1;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i==0&&j==0) continue;
+//                 else{
+//                     int top=0;
+//                     int left=0;
+//                     if(i>0)  top=dp[i-1][j];
+
+//                    if(j>0)  left=dp[i][j-1];
+//                     dp[i][j]= top+left;
+//                 }
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// };
+
+//space
+
 class Solution {
 public:
-int func(int i,int j,vector<vector<int>>& dp){
-    if(i<0 ||j<0) return 0;
-    if(dp[i][j]!=-1) return dp[i][j];
-    if(i==0&&j==0){
-        return 1;
-    }
-  
-    int top=func(i-1,j,dp);
-
-    int left=func(i,j-1,dp);
-    return dp[i][j]=top+left;
-}
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        dp[0][0]=1;
+        vector<int> prev(n,0);
         for(int i=0;i<m;i++){
+        vector<int> cur(n,0);
             for(int j=0;j<n;j++){
-                if(i==0&&j==0) continue;
+                if(i==0&&j==0) cur[j]=1;
                 else{
                     int top=0;
                     int left=0;
-                    if(i>0)  top=dp[i-1][j];
+                    if(i>0)  top=prev[j];
 
-                   if(j>0)  left=dp[i][j-1];
-                    dp[i][j]= top+left;
+                   if(j>0)  left=cur[j-1];
+                    cur[j]= top+left;
                 }
             }
+            prev=cur;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
