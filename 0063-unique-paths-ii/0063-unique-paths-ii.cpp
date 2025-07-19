5f61f2c6fe026512c8b8cf890f85a280349ccb07
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-07-19 17:54:18 +0530

    Time: 3 ms (11.41%), Space: 11.5 MB (86.83%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -25,40 +25,75 @@
 
 
 //tab
-class Solution {
-public:
-int mazeObstaclesUtil(int n, int m, vector<vector<int>> &maze, vector<vector<int>> &dp) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-           if (maze[i][j] == 1) {
-    dp[i][j] = 0;
-    continue;
-}
-            if (i == 0 && j == 0) {
-                dp[i][j] = 1;
-                continue;
-            }
+// class Solution {
+// public:
+// int mazeObstaclesUtil(int n, int m, vector<vector<int>> &maze, vector<vector<int>> &dp) {
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < m; j++) {
+//            if (maze[i][j] == 1) {
+//     dp[i][j] = 0;
+//     continue;
+// }
+//             if (i == 0 && j == 0) {
+//                 dp[i][j] = 1;
+//                 continue;
+//             }
 
-            int up = 0;
-            int left = 0;
+//             int up = 0;
+//             int left = 0;
 
             
-            if (i > 0)
-                up = dp[i - 1][j];
-            if (j > 0)
-                left = dp[i][j - 1];
-            dp[i][j] = up + left;
+//             if (i > 0)
+//                 up = dp[i - 1][j];
+//             if (j > 0)
+//                 left = dp[i][j - 1];
+//             dp[i][j] = up + left;
+//         }
+//     }
+
+//     return dp[n - 1][m - 1];
+// }
+
+//  int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+//     int m=obstacleGrid.size();
+//     int n=obstacleGrid[0].size();
+
+//     vector<vector<int>> dp(m, vector<int>(n, -1)); 
+//     return mazeObstaclesUtil(m, n, obstacleGrid, dp); 
+// }
+//     };
+
+//space
+class Solution {
+public:
+    int mazeObstaclesUtil(int n, int m, vector<vector<int>> &maze) {
+        vector<int> prev(m, 0);
+        for (int i = 0; i < n; i++) {
+            vector<int> cur(m, 0);
+            for (int j = 0; j < m; j++) {
+                if (maze[i][j] == 1) {
+                    cur[j] = 0;
+                    continue;
+                }
+                if (i == 0 && j == 0) {
+                    cur[j] = 1;
+                    continue;
+                }
+
+                int up = 0, left = 0;
+                if (i > 0) up = prev[j];
+                if (j > 0) left = cur[j - 1];
+
+                cur[j] = up + left;
+            }
+            prev = cur; 
         }
+        return prev[m - 1];
     }
 
-    return dp[n - 1][m - 1];
-}
-
- int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-    int m=obstacleGrid.size();
-    int n=obstacleGrid[0].size();
-
-    vector<vector<int>> dp(m, vector<int>(n, -1)); 
-    return mazeObstaclesUtil(m, n, obstacleGrid, dp); 
-}
-    };
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        return mazeObstaclesUtil(n, m, obstacleGrid);
+    }
+};
