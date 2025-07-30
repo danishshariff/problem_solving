a364926c3b6f82d71da4257cf8738a5b825ddf7f
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-07-30 23:06:07 +0530

    Time: 3 ms (43.07%), Space: 9.3 MB (57.76%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+   bool isIsomorphic(string s, string t) {
+    if (s.size() != t.size()) return false;
+
+    unordered_map<char, char> mapST;
+    unordered_map<char, char> mapTS;
+
+    for (int i = 0; i < s.size(); i++) {
+        char cs = s[i], ct = t[i];
+
+        if (mapST.count(cs) && mapST[cs] != ct) return false;
+        if (mapTS.count(ct) && mapTS[ct] != cs) return false;
+
+        mapST[cs] = ct;
+        mapTS[ct] = cs;
+    }
+
+    return true;
+}
+
+};
