b84c25b4606b52753dc661a4b594027127c3a776
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-08-07 15:39:29 +0530

    Time: 42 ms (22.47%), Space: 92.3 MB (19.78%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.cpp
+++ 0937-online-stock-span/0937-online-stock-span.cpp
@@ -1,23 +1,44 @@
 class StockSpanner {
 public:
-        vector<int> arr;
-    StockSpanner() {
-    }
+    //     vector<int> arr;
+    // StockSpanner() {
+    // }
     
-    int next(int price) {
-        arr.push_back(price);
-        int cnt=1;
-        for(int i=arr.size()-2;i>=0;i--){
-            if(arr[i]<=price){
-                cnt++;
+    // int next(int price) {
+    //     arr.push_back(price);
+    //     int cnt=1;
+    //     for(int i=arr.size()-2;i>=0;i--){
+    //         if(arr[i]<=price){
+    //             cnt++;
                 
-            }
-            else{
-                break;
-            }
-        }
-        return cnt;
+    //         }
+    //         else{
+    //             break;
+    //         }
+    //     }
+    //     return cnt;
+    // }
+
+   stack<pair<int,int>> st;
+   int ind=-1;
+    void stclear(std::stack<std::pair<int, int>>& s) {
+        s = std::stack<std::pair<int, int>>();
     }
+    StockSpanner() {
+        ind=-1;
+        stclear(st);
+    }
+
+        int next(int price) {
+            ind=ind+1;
+            while(!st.empty()&& st.top().first<=price){
+                st.pop();
+            }
+            int ans = st.empty() ? ind + 1 : ind - st.top().second;
+            st.push({price,ind});
+            return ans;
+        }
+    
 };
 
 /**
