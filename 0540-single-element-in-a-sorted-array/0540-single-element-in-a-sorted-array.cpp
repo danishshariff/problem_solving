99a01314973ffdf78b950368f9b6ba381d084945
Danish Neman Sharieff <132253347+danishshariff@users.noreply.github.com>
2025-08-27 14:16:07 +0530

    Time: 0 ms (100%), Space: 26.1 MB (91.84%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -24,22 +24,28 @@ public:
     //     }
     // return -1;
 
-     if(n==1) return nums[0];
-     if(nums[0]!=nums[1]) return nums[0];
-     if(nums[n-1]!=nums[n-2]) return nums[n-1];
-     int low=1,high=n-2;
-     while(low<=high){
-        int mid=(low+high)/2;
-        if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]){
-            return nums[mid];
-        }
-        if((mid%2==1 && nums[mid-1]==nums[mid])||(mid%2==0 && nums[mid]==nums[mid+1])){
-            low=mid+1;
-        }
-        else{
-            high=mid-1;
-        }
-     }
-     return -1;
+    //  if(n==1) return nums[0];
+    //  if(nums[0]!=nums[1]) return nums[0];
+    //  if(nums[n-1]!=nums[n-2]) return nums[n-1];
+    //  int low=1,high=n-2;
+    //  while(low<=high){
+    //     int mid=(low+high)/2;
+    //     if(nums[mid]!=nums[mid-1]&&nums[mid]!=nums[mid+1]){
+    //         return nums[mid];
+    //     }
+    //     if((mid%2==1 && nums[mid-1]==nums[mid])||(mid%2==0 && nums[mid]==nums[mid+1])){
+    //         low=mid+1;
+    //     }
+    //     else{
+    //         high=mid-1;
+    //     }
+    //  }
+    //  return -1;
+
+    long xoir=0;
+    for(int i=0;i<nums.size();i++){
+        xoir=xoir^nums[i];
+    }
+    return xoir;
     }
 };
